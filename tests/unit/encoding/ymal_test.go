18051bb1760cab5ed6d98f1471b1edf31a2ef6a7
Frostre_ <n9043395@gmail.com>
2026-09-05 12:17:42 +0200

    Add ymal_test.go to encoding tests

---
+++ tests/unit/encoding/ymal_test.go
@@ -0,0 +1,30 @@
+package encoding
+
+import (
+    "testing"
+
+    "github.com/Denvos/core/encoding/yaml"
+)
+
+type testStruct struct {
+    Name string `yaml:"name"`
+}
+
+func TestYAMLCodec(t *testing.T) {
+    c := yaml.Codec{}
+    v := testStruct{Name: "test"}
+
+    data, err := c.Marshal(v)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    var out testStruct
+    if err := c.Unmarshal(data, &out); err != nil {
+        t.Fatal(err)
+    }
+
+    if out.Name != "test" {
+        t.Errorf("expected test, got %s", out.Name)
+    }
+}
