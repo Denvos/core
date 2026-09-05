90f97991716c7bb5b3fd40e4404e4dfbfe2c211f
Frostre_ <n9043395@gmail.com>
2026-09-05 12:20:18 +0200

    Implement JSON codec with Marshal and Unmarshal methods

---
+++ encoding/json/json.go
@@ -0,0 +1,23 @@
+package json
+
+import (
+    "encoding/json"
+)
+
+type Codec struct{}
+
+func (c Codec) Marshal(v interface{}) ([]byte, error) {
+    return json.Marshal(v)
+}
+
+func (c Codec) Unmarshal(data []byte, v interface{}) error {
+    return json.Unmarshal(data, v)
+}
+
+func (c Codec) ContentType() string {
+    return "application/json"
+}
+
+func init() {
+    encoding.Register("json", Codec{})
+}
