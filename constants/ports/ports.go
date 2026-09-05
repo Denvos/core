efc4ae4cb523f1d76db993b537d9010539d545a6
Frostre_ <n9043395@gmail.com>
2026-09-05 16:23:36 +0200

    Add ports.go with defined port constants

---
+++ constants/ports/ports.go
@@ -0,0 +1,18 @@
+package ports
+
+const (
+	HTTP  = 80
+	HTTPS = 443
+)
+
+const (
+	DenvosAPIPort     = 8080
+	DenvosGRPCPort    = 9090
+	DenvosMetricsPort = 9091
+	DenvosWebPort     = 3000
+)
+
+const (
+	MinPort = 1
+	MaxPort = 65535
+)
