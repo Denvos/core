7845888ad113776b05a2e4725caece39d37d8e31
Frostre_ <n9043395@gmail.com>
2026-09-05 16:02:45 +0200

    Refactor Datadog logger with options and API client

--- logger/datadog/datadog.go
+++ logger/datadog/datadog.go
@@ -1,8 +1,84 @@
 package datadog
 
-import "io"
+import (
+	"context"
+	"io"
 
-type Datadog struct{ w io.Writer }
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
+)
 
-func New(w io.Writer) *Datadog { return &Datadog{w: w} }
-func (d *Datadog) Write(p []byte) (int, error) { return d.w.Write(p) }
+type Datadog struct {
+	client   *datadogV2.LogsApi
+	ctx      context.Context
+	service  string
+	source   string
+	hostname string
+	tags     []string
+}
+
+type Option func(*Datadog)
+
+func WithService(service string) Option {
+	return func(d *Datadog) {
+		d.service = service
+	}
+}
+
+func WithSource(source string) Option {
+	return func(d *Datadog) {
+		d.source = source
+	}
+}
+
+func WithHostname(hostname string) Option {
+	return func(d *Datadog) {
+		d.hostname = hostname
+	}
+}
+
+func WithTags(tags []string) Option {
+	return func(d *Datadog) {
+		d.tags = tags
+	}
+}
+
+func New(opts ...Option) (*Datadog, error) {
+	ctx := context.Background()
+	cfg := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(cfg)
+	api := datadogV2.NewLogsApi(apiClient)
+
+	d := &Datadog{
+		client: api,
+		ctx:    ctx,
+	}
+	for _, opt := range opts {
+		opt(d)
+	}
+	return d, nil
+}
+
+func (d *Datadog) Write(p []byte) (int, error) {
+	log := datadogV2.NewHTTPLogItem()
+	log.SetMessage(string(p))
+
+	if d.service != "" {
+		log.SetService(d.service)
+	}
+	if d.source != "" {
+		log.SetSource(d.source)
+	}
+	if d.hostname != "" {
+		log.SetHostname(d.hostname)
+	}
+	if len(d.tags) > 0 {
+		log.SetTags(d.tags)
+	}
+
+	_, _, err := d.client.SubmitLog(d.ctx, []datadogV2.HTTPLogItem{*log})
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
+}
